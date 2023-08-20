5c4167ded2979d36d8f2061250ca5f6ec2db94bd
kshanu12 <kumarshanu38370@gmail.com>
2023-08-21 01:40:45 +0530

    added button and modal component

--- frontend/components/layout/followBar.tsx
+++ frontend/components/layout/followBar.tsx
@@ -3,7 +3,12 @@ const FollowBar = () => {
         <div className="px-6 py-4 hidden lg:block">
             <div className="bg-neutral-800 rounded-xl p-4">
                 <h2 className="text-white text-xl font-semibold">Who to Follow</h2>
-                <div className="flex flexx-col gap-6 mt-4"></div>
+                <div className="flex flex-col gap-6 mt-4">
+                    <p>Lorem, ipsum.</p>
+                    <p>Lorem, ipsum.</p>
+                    <p>Lorem, ipsum.</p>
+                    <p>Lorem, ipsum.</p>
+                </div>
             </div>
         </div>
     )
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,15 @@
-import Layout from '@/components/layout/layout'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Layout from "@/components/layout";
+import Modal from "@/components/modal";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <>
+      <Modal actionLabel="Submit" isOpen title="Test Modal"/>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
